1b38e43aec5e2dd4f6a4bf4b8efea2e9cc23d0a2
Wilson Wong <wilwong89@gmail.com>
2023-01-10 08:22:46 -0800

    BRS-950: Updated unit tests

--- __tests__/park.test.js
+++ __tests__/park.test.js
@@ -1,13 +1,13 @@
 const AWS = require('aws-sdk');
 const { DocumentClient } = require('aws-sdk/clients/dynamodb');
 const { REGION, ENDPOINT, TABLE_NAME } = require('./global/settings');
-const { PARKSLIST, SUBAREAS } = require('./global/data.json');
+const { FISCAL_YEAR_LOCKS } = require('./global/data.json');
 
-const parkGET = require('../lambda/park/GET/index');
-const parkPOST = require('../lambda/park/POST/index');
+const fiscalYearEndGET = require('../lambda/fiscalYearEnd/GET/index');
+const fiscalYearEndPOST = require('../lambda/fiscalYearEnd/POST/index');
 
 const jwt = require('jsonwebtoken');
-const token = jwt.sign({ resource_access: { 'attendance-and-revenue': { roles: ['sysadmin']}} }, 'defaultSecret');
+const token = jwt.sign({ resource_access: { 'attendance-and-revenue': { roles: ['sysadmin'] } } }, 'defaultSecret');
 
 async function setupDb() {
   new AWS.DynamoDB({
@@ -20,109 +20,86 @@ async function setupDb() {
     convertEmptyValues: true
   });
 
-  for(const park of PARKSLIST) {
-    await docClient
-      .put({
-        TableName: TABLE_NAME,
-        Item: park
-      })
-      .promise();
-  }
-
-  for (const subarea of SUBAREAS) {
-    await docClient
-      .put({
-        TableName: TABLE_NAME,
-        Item: subarea
-      })
-      .promise();
+  for (const item of FISCAL_YEAR_LOCKS) {
+    await (genericPutDocument(item));
   }
 }
 
-describe('Pass Succeeds', () => {
+async function genericPutDocument(item) {
+  return await docClient
+    .put({
+      TableName: TABLE_NAME,
+      Item: item
+    })
+    .promise();
+}
+
+describe('Fiscal Year End Test', () => {
   beforeAll(async () => {
     return await setupDb();
   });
 
-  test('Handler - 200 Received list of parks', async () => {
-    const event = {
+  test('Handler - 200 GET fiscal year end', async () => {
+    const obj = await fiscalYearEndGET.handler(
+      {
+        headers: {
+          Authorization: "Bearer " + token
+        },
+        queryStringParameters: {
+          fiscalYearEnd: FISCAL_YEAR_LOCKS[0].sk
+        }
+      }, null);
+    expect(JSON.parse(obj.body)).toMatchObject(FISCAL_YEAR_LOCKS[0]);
+  });
+
+  test('Handler - 200 GET All fiscal year end objects', async () => {
+    const response = await fiscalYearEndGET.handler({
       headers: {
         Authorization: "Bearer " + token
       },
-      httpMethod: "GET"
-    };
-    expect(await parkGET.handler(event, null)).toMatchObject({
-      body: JSON.stringify(PARKSLIST),
-      headers: {
-        'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
-        'Access-Control-Allow-Methods': 'OPTIONS,GET,POST',
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json'
-      },
-      statusCode: 200
-    });
+    }, null);
+
+    expect(response.statusCode).toBe(200);
   });
 
-  test('Handler - 200 Receive park specific information', async () => {
-    let specificSubAreas = [];
-    for(const area of SUBAREAS) {
-      if (area.pk === "park::0041") {
-        specificSubAreas.push(area);
-      }
-    }
-    const response = await parkGET.handler({
+  test('HandleLock - 200 lock fiscal year', async () => {
+    const response = await fiscalYearEndPOST.lockFiscalYear({
       headers: {
         Authorization: "Bearer " + token
       },
       queryStringParameters: {
-        orcs: PARKSLIST[0].sk,
-        subAreaId: specificSubAreas[0].sk
+        fiscalYearEnd: "2018"
       }
     }, null);
 
-    const body = JSON.parse(response.body);
-
-    expect(body.data[0].subAreaName).toMatch(body.data[0].subAreaName);
+    expect(response.statusCode).toBe(200);
   });
 
-  test('Handler - 400 GET Bad Request', async () => {
-    const response = await parkGET.handler({
-      headers: {
-        Authorization: "Bearer " + token + "invalid"
-      },
-      queryStringParameters: {
-        badParam: "oops"
-      }
-    }, null);
-
-    expect(response.statusCode).toBe(400);
-  });
-
-  test('Handler - 400 POST Bad Request', async () => {
-    const response = await parkPOST.handler({
-      headers: {
-        Authorization: "Bearer " + token + "invalid"
-      },
-      body: JSON.stringify({
-        badParam: "{xxxxxx}"
-      })
-    }, null);
-
-    expect(response.statusCode).toBe(400);
-  });
-
-  test('Handler - 200 POST Park', async () => {
-    const response = await parkPOST.handler({
+  test('HandleLock - 200 unlock fiscal year', async () => {
+    const response = await fiscalYearEndPOST.unlockFiscalYear({
       headers: {
         Authorization: "Bearer " + token
       },
-      body: JSON.stringify({
-        orcs: '0000',
-        someconfig: "test"
-      })
+      queryStringParameters: {
+        fiscalYearEnd: "2018"
+      }
     }, null);
 
-    expect(response.statusCode).toBe(400);
+    expect(response.statusCode).toBe(200);
   });
-  
+
+  test('HandleLock - 403 unlock fiscal year without perms', async () => {
+    const response = await fiscalYearEndPOST.unlockFiscalYear({
+      headers: {
+        Authorization: "Bearer " + token,
+        PsuedoToken: { resource_access: { 'attendance-and-revenue': { roles: [''] } } }
+      },
+      queryStringParameters: {
+        fiscalYearEnd: "2018"
+      }
+    }, null);
+
+    expect(response.statusCode).toBe(403);
+  });
+
 });
