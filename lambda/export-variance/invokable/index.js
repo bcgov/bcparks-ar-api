bd9563554e5fd2cc301e2cd05d8c87180fdf5644
Christopher-walsh22 <106549296+Christopher-walsh22@users.noreply.github.com>
2023-10-26 16:59:25 +0000

    Add switch to convert month number to names.

--- lambda/export-variance/invokable/index.js
+++ lambda/export-variance/invokable/index.js
@@ -236,7 +236,7 @@ function formatRecords(records) {
     }
     const date = record.pk.split('::')[2];
     record['year'] = date.slice(0, 4);
-    record['month'] = date.slice(4);
+    record['month'] = convertMonth(parseInt(date.slice(4)));
   }
 }
 
@@ -282,4 +282,50 @@ async function uploadToS3(csvData) {
   }
   logger.debug("Uploaded to S3");
 
+}
+
+function convertMonth(monthNumber){
+  let monthName;
+
+  switch (monthNumber) {
+    case 1:
+      monthName = 'January';
+      break;
+    case 2:
+      monthName = 'February';
+      break;
+    case 3:
+      monthName = 'March';
+      break;
+    case 4:
+      monthName = 'April';
+      break;
+    case 5:
+      monthName = 'May';
+      break;
+    case 6:
+      monthName = 'June';
+      break;
+    case 7:
+      monthName = 'July';
+      break;
+    case 8:
+      monthName = 'August';
+      break;
+    case 9:
+      monthName = 'September';
+      break;
+    case 10:
+      monthName = 'October';
+      break;
+    case 11:
+      monthName = 'November';
+      break;
+    case 12:
+      monthName = 'December';
+      break;
+    default:
+      monthName = `Invalid month: ${monthNumber}`;
+  }
+  return monthName
 }
