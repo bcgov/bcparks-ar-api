c4f0378eb4c64763c70b6cead61826e294d61ac1
Christopher-walsh22 <106549296+Christopher-walsh22@users.noreply.github.com>
2024-02-06 22:12:20 +0000

    Conform to sonar cloud for CheckRecords script

--- migrations-data/checkAllRecordsForORC.js
+++ migrations-data/checkAllRecordsForORC.js
@@ -24,35 +24,7 @@ exports.up = async function(oldORC){
         if(parkData.length > 0){
             console.log("Park exists: ", oldORC);
             console.log("Checking for subareas of: ", oldORC)
-            const subAreaList = await getSubAreas(oldORC);
-            if (subAreaList.length > 0){
-                console.log(oldORC, " Has subAreas.")
-                for(const area of subAreaList){ 
-                    console.log("Subarea exists: ", area.pk, "::", area.sk)
-                    const activityList = area.activities.values
-                    for(const activity of activityList){
-                        const config = await getConfigs(area);
-                        if(config.length > 0){
-                            console.log("Config exists: config::", area.sk)
-                        }
-                    }
-                    for (const activity of activityList){
-                        const recordsList = await getActivityRecords(area, activity);
-                        if(recordsList.length > 0){
-                            console.log(area.pk,"::", area.sk, " Has records.")
-                            for (const record of recordsList) {
-                                console.log("Record exists", record.pk, " ", record.sk)
-                                const varianceList = await getVariances(record, oldORC); // Access each object using recordsArray[record]
-                                for (const variance of varianceList){
-                                    console.log("Variance exists: ", variance.pk, " ", variance.sk)
-                                }    
-                            }
-                        }
-                    }
-                }
-            }else{
-                console.log(oldORC, " Has no subAreas.")
-            }
+            hasSubAreas(oldORC);   
         }else{
             console.log("Park does not exist: ", oldORC);
         }
@@ -61,7 +33,49 @@ exports.up = async function(oldORC){
         console.error('Error:', error);
     }    
 }
+async function hasSubAreas(oldORC){
+    const subAreaList = await getSubAreas(oldORC);
+    if (subAreaList.length > 0){
+        console.log(oldORC, " Has subAreas.")
+        for(const area of subAreaList){     
+            console.log("Subarea exists: ", area.pk, "::", area.sk)
+            printConfigs(area);
+            printRecords(area, oldORC);
+        }
+    }else{
+        console.log(oldORC, " Has no subAreas.")
+    }
+}
 
+async function printRecords(area, oldORC){
+    const activityList = area.activities.values 
+    for ( activity of activityList ) {
+        const recordsList = await getActivityRecords(area, activity);
+        if(recordsList.length > 0){
+            console.log(area.pk,"::", area.sk, " Has records.")
+            for (const record of recordsList) {
+                console.log("Record exists", record.pk, " ", record.sk)
+                printVariance(record, oldORC);                    
+            }
+        }
+    }
+}
+
+async function printVariance(record, oldORC){
+    const varianceList = await getVariances(record, oldORC);
+    for (const variance of varianceList){
+        console.log("Variance exists: ", variance.pk, " ", variance.sk)
+    }    
+}
+async function printConfigs(area){
+    const activityList = area.activities.values           
+    for( activity of activityList ) {
+        const config = await getConfigs(area);
+        if(config.length > 0){
+            console.log("Config exists: config::", area.sk)
+        }
+    }
+}
 async function getPark(orc) {
     const getPark = {
         TableName: TABLE_NAME,
@@ -88,17 +102,17 @@ async function getConfigs(subArea) {
 }
 
 async function getActivityRecords(subArea, activity) {
-  const subAreaId = subArea.sk;
-  let activityRecords = [];
-  const getActivitiesQuery = {
-      TableName: TABLE_NAME,
-      KeyConditionExpression: "pk = :pk",
-      ExpressionAttributeValues: {
-          ":pk": { S: `${subAreaId}::${activity}` },
-      },
-  };
-activityRecords = await runQuery(getActivitiesQuery);
-return activityRecords;
+    const subAreaId = subArea.sk;
+    let activityRecords = [];
+    const getActivitiesQuery = {
+        TableName: TABLE_NAME,
+        KeyConditionExpression: "pk = :pk",
+        ExpressionAttributeValues: {
+            ":pk": { S: `${subAreaId}::${activity}` },
+        },
+    };
+    activityRecords = await runQuery(getActivitiesQuery);
+    return activityRecords;
 }
   
 async function getVariances(record, ORC) {
