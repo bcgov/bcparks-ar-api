121dd1e55f906c1c6df9e24f69b32d2ca439ccd7
Dave <62899351+davidclaveau@users.noreply.github.com>
2024-07-30 14:04:27 -0700

    Update settings.js
    fix to be localhost

--- arSam/__tests__/settings.js
+++ arSam/__tests__/settings.js
@@ -1,5 +1,5 @@
 const REGION = process.env.AWS_REGION || 'local-env';
-const ENDPOINT = 'http://172.17.0.2:8000';
+const ENDPOINT = 'http://localhost:8000';
 const TABLE_NAME = process.env.TABLE_NAME || 'ParksAr-tests';
 const CONFIG_TABLE_NAME = process.env.CONFIG_TABLE_NAME || 'ConfigAr-tests';
 const NAME_CACHE_TABLE_NAME = process.env.NAME_CACHE_TABLE_NAME || 'NameCacheAr-tests';
