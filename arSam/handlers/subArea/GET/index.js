699b922c085dd2710d47d21e202110349e41783f
Cameron Pettit <71421099+cameronpettit@users.noreply.github.com>
2025-03-12 17:09:06 -0700

    AR-427-4 Adding OPTIONS handling method to Subarea GET (#402)
    * fixing bundle path
    
    * adding OPTIONS to subarea GET (not implemented)
    
    Force merging cuz this already should have been done...

--- arSam/handlers/subArea/GET/index.js
+++ arSam/handlers/subArea/GET/index.js
@@ -1,8 +1,12 @@
 // TODO: Decouple subArea get from park get endpoint.
-const { logger } = require("/opt/baseLayer");
+const { logger, sendResponse } = require("/opt/baseLayer");
 
 exports.handler = async (event, context) => {
   logger.debug("Subarea get:", event);
+
+  if (event.httpMethod === 'OPTIONS') {
+    return sendResponse(200, {}, context);
+  }
+
   return sendResponse(501, { msg: "Error: Not implemented." }, context);
 };
-   
