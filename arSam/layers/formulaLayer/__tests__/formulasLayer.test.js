0cb61dc3f7d68aabe7c113efa17cfde45f269c0c
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2026-07-20 17:23:50 -0700

    chore: fixed non-resident revenue calculation

--- arSam/layers/formulaLayer/__tests__/formulasLayer.test.js
+++ arSam/layers/formulaLayer/__tests__/formulasLayer.test.js
@@ -44,15 +44,15 @@ describe("keycloak utility tests", () => {
       ]);
     });
 
-    test("Calculates non-resident net revenue using gross x 1.05", async () => {
+    test("Calculates non-resident net revenue using gross / 1.05", async () => {
       const utils = require("../formulaLayer");
 
-      expect(utils.nonResidentNetRevenue([100]).result).toBe(105);
-      expect(utils.nonResidentNetRevenue([50, 25]).result).toBe(78.75);
+      expect(utils.nonResidentNetRevenue([100]).result).toBe(95.24);
+      expect(utils.nonResidentNetRevenue([50, 25]).result).toBe(71.43);
       expect(utils.nonResidentNetRevenue([0]).result).toBe(0);
       expect(utils.nonResidentNetRevenue([NaN]).result).toBeNull();
       expect(utils.nonResidentNetRevenue([100]).formula).toBe(
-        "Non-resident net revenue = Gross non-resident revenue x 1.05"
+        "Non-resident net revenue = Gross non-resident revenue / 1.05"
       );
     });
   });
