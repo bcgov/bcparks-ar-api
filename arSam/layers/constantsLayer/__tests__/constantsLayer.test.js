0546cbd6a1f06a8cf84b21f50b0ced4c8631fcb1
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2026-08-10 17:19:19 -0700

    chore: fixed test

--- arSam/layers/constantsLayer/__tests__/constantsLayer.test.js
+++ arSam/layers/constantsLayer/__tests__/constantsLayer.test.js
@@ -9,7 +9,7 @@ describe("Constants Test", () => {
   
       const constants = require("../constantsLayer");
       // Checks to ensure the value functions returns the data we pass through to it based on the attribute.
-      expect(constants.CSV_SYSADMIN_SCHEMA.length).toEqual(116);
+      expect(constants.CSV_SYSADMIN_SCHEMA.length).toEqual(118);
       for(const row of constants.CSV_SYSADMIN_SCHEMA) {
         expect(row.value({
           region: 1,
